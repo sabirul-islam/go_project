139ea43ec5de9e266152a978b86ba792f1609d66
sabirul-islam <sabirul.shimul@gmail.com>
2021-01-29 23:20:06 +0600

    first go code

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println(
+	`Animesh Dhar
+	Student of Mba
+	Bangabandhu Sheikh Mujibar Rahman Science And Technology University
+	Graphic Designer
+	Social Worker`)
+}
