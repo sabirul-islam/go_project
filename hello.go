c541d05d152512b69946457b05894ac950f45e67
sabirul-islam <sabirul.shimul@gmail.com>
2021-02-01 22:21:46 +0600

    if else check

--- hello.go
+++ hello.go
@@ -11,11 +11,19 @@ func main(){
 	// Social Worker`)
 
 	// var name string = "shimul"
-	var name, city string = "shimul", "jamalpur"
+	var name, city string = "shimul", "sarishabari"
 	
-	var c rune = 'A'
-	var age int = 10
-	var result float32 = 50.95
+	// var c rune = 'A'
+	// var age int = 10
+	// var result float32 = 50.95
 
-	fmt.Println(name, city, c, age, result)
+	// fmt.Println(name, city, c, age, result)
+
+
+
+	if (name == "shimul" && city == "jamalpur") {
+		fmt.Println("He is jamalpuira")
+	}else{
+		fmt.Println("He is not jamalpuira")
+	}
 }
