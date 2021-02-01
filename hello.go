ec585b8255621e0c3c0639f4fc4d2f52ad82927e
sabirul-islam <sabirul.shimul@gmail.com>
2021-02-01 19:05:57 +0600

    practicing variable

--- hello.go
+++ hello.go
@@ -1,11 +1,21 @@
 package main
+
 import "fmt"
 
 func main(){
-	fmt.Println(
-	`Animesh Dhar
-	Student of Mba
-	Bangabandhu Sheikh Mujibar Rahman Science And Technology University
-	Graphic Designer
-	Social Worker`)
+	// fmt.Println(
+	// `Animesh Dhar
+	// Student of Mba
+	// Bangabandhu Sheikh Mujibar Rahman Science And Technology University
+	// Graphic Designer
+	// Social Worker`)
+
+	// var name string = "shimul"
+	var name, city string = "shimul", "jamalpur"
+	
+	var c rune = 'A'
+	var age int = 10
+	var result float32 = 50.95
+
+	fmt.Println(name, city, c, age, result)
 }
